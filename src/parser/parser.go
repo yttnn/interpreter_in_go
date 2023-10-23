64f9df9814e9e3ccbac49f092cf6127d771bf8d2
yttnn <67893139+yttnn@users.noreply.github.com>
2023-10-23 18:02:20 +0900

    to p.35 ( add paser )

---
+++ src/parser/parser.go
@@ -0,0 +1,31 @@
+package parser
+
+import (
+	"monkey/src/ast"
+	"monkey/src/lexer"
+	"monkey/src/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
